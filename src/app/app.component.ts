ca8a380f8e44bae9c4182e53870beaa82f476c31
Edu Gutierrez <edu.gutierrez.navarro@gmail.com>
2021-03-15 23:19:47 +0100

    Números de los Pokemon Añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class AppComponent {
       hp: data.stats[0].base_stat,
       attack: data.stats[1].base_stat,
       defense: data.stats[2].base_stat,
+      number: random
     }
     this.pokemons.push(pokemon)
   }
